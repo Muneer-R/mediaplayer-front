4d01f363fb6354b8464ba83e70e66cfc12891775
Muneer-R <muneerrahmaniya@gmail.com>
2023-12-14 13:01:09 +0530

    front

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Upload } from 'react-feather'
+
+function Footer() {
+  return (
+    <div>
+
+    <p className='text-center'>All Rights Reserved &copy;  <Upload/> <b>VLC Player</b> &reg; 2023</p>
+
+    </div>
+  )
+}
+
+export default Footer
+
+
